20f37b334c7864a7d6c707237d26d8baecde0a72
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-24 11:45:21 +0700

    Fix login

--- login.php
+++ login.php
@@ -7,7 +7,29 @@ if (isset($_POST['login'])) {
 	} else {
 		$login = false;
 	}
+} else {
+	$login = false;
 }
 if ($login) {
-	setcookie("login", base64_encode(Teacrypt::encrypt($_POST['username'], "crayner")), );
+	setcookie("login", base64_encode(Teacrypt::encrypt($_POST['username'], "crayner")), time()+7200);
+	header("location:?ref=login");
+	die;
 }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+<center>
+	<form method="post" action="">
+		<label>Username : </label><br>
+		<input type="text" name="username"><br><br>
+		<label>Password : </label><br>
+		<input type="password" name="password"><br><br>
+		<input type="submit" name="login" value="login">
+	</form>
+</center>
+</body>
+</html>
